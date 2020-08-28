ab6fca16af858a632614d9b4ad813c92fdb783a7
mike quong <m.quong@.sap.corp>
2020-08-27 18:32:54 -0700

    New creation scripts for city and asn mmdbs
    Added embedded documentations. Ensure you have the latest perldocs for
    your flavour of unix/linux installed to view --help properly.
    
    execution: perl <script> <flags>
    Documentation: perl <script> -h|--help
    
    Scripts requires an city.json/asn.json files to support custom curation
    of mmdb.
    To include Public network info, also pass --public path/to/maxmind.mmdb
    to also include their content as well. WARNING: Including Public
    networks increases generation time in the HOURS.

--- openstack/elastiflow/files/create_geoip_asn.pl
+++ openstack/elastiflow/files/create_geoip_asn.pl
@@ -8,51 +8,34 @@ use local::lib 'local';
 use MaxMind::DB::Reader;
 use MaxMind::DB::Writer::Tree;
 use Net::Works::Network;
+use JSON::PP;
+use Getopt::Long;
+use Pod::Usage;
 
-my $filename = 'GeoLite2-ASN-SAP.mmdb';
-my $reader   = MaxMind::DB::Reader->new( file => 'GeoLite2-ASN.mmdb' );
+my $help;
+my $json_file = '';
+my $maxmind = '';
+my $filename = 'GeoLite2-ASN-SAP-test.mmdb';
+GetOptions ("output=s" => \$filename,
+            "file=s"   => \$json_file,
+			"public=s" => \$maxmind,
+            "help"  => \$help)   # flag
+or pod2usage("Try 'perl $0 --help' for more information.");
+
+pod2usage( -verbose => 2 ) if $help || $json_file eq '';
 
 my %types = (
     autonomous_system_number        => 'uint32',
     autonomous_system_organization  => 'utf8_string',
 );
 
-my %address_for_sap_asn = (
-    '10.216.24.0/23'  => asn(4268359786, 'SAP ConvergedCloud QA-DE-1 CC-CLOUD06'),
-    '10.236.178.0/24' => asn(4268359782, 'SAP ConvergedCloud QA-DE-1 CC-CLOUD02'),
-    '10.236.208.0/21' => asn(4268359781, 'SAP ConvergedCloud QA-DE-1 CC-CLOUD01'),
-    '10.236.36.0/22'  => asn(4268359781, 'SAP ConvergedCloud QA-DE-1 CC-CLOUD01'),
-    '10.236.40.0/22'  => asn(4268359782, 'SAP ConvergedCloud QA-DE-1 CC-CLOUD02'),
-    '10.236.44.0/22'  => asn(4268359783, 'SAP ConvergedCloud QA-DE-1 CC-CLOUD03'),
-    '10.236.48.0/22'  => asn(4268359784, 'SAP ConvergedCloud QA-DE-1 CC-CLOUD04'),
-    '10.236.52.0/22'  => asn(4268359785, 'SAP ConvergedCloud QA-DE-1 CC-CLOUD05'),
-    '10.246.239.0/24' => asn(4268360580, 'SAP ConvergedCloud QA-DE-1 CC-MGMT'),
-    '10.246.26.0/24'  => asn(4268360580, 'SAP ConvergedCloud QA-DE-1 CC-MGMT'),
-    '10.246.3.0/24'   => asn(4268360580, 'SAP ConvergedCloud QA-DE-1 CC-MGMT'),
-    '10.46.100.0/23'  => asn(4268360580, 'SAP ConvergedCloud QA-DE-1 CC-MGMT'),
-    '10.46.102.0/24'  => asn(4268360580, 'SAP ConvergedCloud QA-DE-1 CC-MGMT'),
-    '10.46.104.0/23'  => asn(4268360580, 'SAP ConvergedCloud QA-DE-1 CC-MGMT'),
-    '10.46.106.0/24'  => asn(4268360580, 'SAP ConvergedCloud QA-DE-1 CC-MGMT'),
-    '10.46.115.0/24'  => asn(4268360580, 'SAP ConvergedCloud QA-DE-1 CC-MGMT'),
-    '10.46.116.0/23'  => asn(4268360580, 'SAP ConvergedCloud QA-DE-1 CC-MGMT'),
-    '10.46.178.0/24'  => asn(4268360580, 'SAP ConvergedCloud QA-DE-1 CC-MGMT'),
-    '10.46.192.0/24'  => asn(4268360580, 'SAP ConvergedCloud QA-DE-1 CC-MGMT'),
-    '10.46.209.0/24'  => asn(4268360580, 'SAP ConvergedCloud QA-DE-1 CC-MGMT'),
-    '10.46.68.0/22'   => asn(4268360580, 'SAP ConvergedCloud QA-DE-1 CC-MGMT'),
-    '130.214.232.0/28'   => asn(4268359782, 'SAP ConvergedCloud QA-DE-1 CC-CLOUD02'),
-    '130.214.232.128/26' => asn(4268359783, 'SAP ConvergedCloud QA-DE-1 CC-CLOUD03'),
-    '130.214.232.16/28'  => asn(4268360580, 'SAP ConvergedCloud QA-DE-1 CC-MGMT'),
-    '130.214.232.192/26' => asn(4268359784, 'SAP ConvergedCloud QA-DE-1 CC-CLOUD04'),
-    '130.214.232.32/27'  => asn(4268360580, 'SAP ConvergedCloud QA-DE-1 CC-MGMT'),
-    '130.214.232.64/26'  => asn(4268359782, 'SAP ConvergedCloud QA-DE-1 CC-CLOUD02'),
-    '130.214.233.0/26'   => asn(4268359785, 'SAP ConvergedCloud QA-DE-1 CC-CLOUD05'),
-    '130.214.233.64/28'  => asn(4268359783, 'SAP ConvergedCloud QA-DE-1 CC-CLOUD03'),
-    '130.214.233.96/27'  => asn(4268359782, 'SAP ConvergedCloud QA-DE-1 CC-CLOUD02'),
-    '147.204.198.0/24'   => asn(4268359786, 'SAP ConvergedCloud QA-DE-1 CC-CLOUD06'),
-    '147.204.35.128/27'  => asn(4268360580, 'SAP ConvergedCloud QA-DE-1 CC-MGMT'),
-    '147.204.35.160/27'  => asn(4268359786, 'SAP ConvergedCloud QA-DE-1 CC-CLOUD06'),
-);
-
+my $address_for_sap_asn;
+{
+	open (my $json_fh, "<:raw", $json_file) or die("can't open \$json_file\" $!\n");
+	local $/ = undef;
+	$address_for_sap_asn = decode_json(<$json_fh>);
+	close $json_fh;
+}
 
 my $tree = MaxMind::DB::Writer::Tree->new(
     database_type => 'GeoIP2-ASN-SAP',
@@ -61,46 +44,110 @@ my $tree = MaxMind::DB::Writer::Tree->new(
     ip_version => 6,
     map_key_type_callback => sub { $types{ $_[0] } },
     merge_strategy => 'recurse',
-    record_size => 24,
+    record_size => 32,
     # Disable reservation of private networks in mmdbs
     remove_reserved_networks => 0,
 );
 
-# Maxmind Public IPs from GeoLite2-ASN.mmdb
-$reader->iterate_search_tree(
-    sub {
-        my $ip_as_integer = shift;
-        my $mask_length   = shift;
-        my $data          = shift;
+if ( $maxmind ne '' ) {
+	my $reader   = MaxMind::DB::Reader->new( file => $maxmind );
+	say "Insert Public Network Addresses from MaxMind DB";
+	# Maxmind Public IPs from GeoLite2-ASN.mmdb
+	$reader->iterate_search_tree(
+		sub {
+			my $ip_as_integer = shift;
+			my $mask_length   = shift;
+			my $data          = shift;
 
-        my $address = Net::Works::Address->new_from_integer(integer => $ip_as_integer );
-        my $ip_version = $address->version;
+			my $address = Net::Works::Address->new_from_integer(integer => $ip_as_integer );
+			my $ip_version = $address->version;
 
-        my $network = join '/', $address->as_string, $mask_length;
-        $tree->insert_network($network, $data);
-    }
-);
+			my $network = join '/', $address->as_string, $mask_length;
+			say "Insert Network $network";
+			$tree->insert_network($network, $data);
+		}
+	);
+}
 
 # SAP Custom Private IPs
-for my $range (keys %address_for_sap_asn) {
+for my $range (keys %{$address_for_sap_asn}) {
     my $network = Net::Works::Network->new_from_string( string => $range );
-    my $data = $address_for_sap_asn{$range};
+    my $data = $address_for_sap_asn->{$range};
 
     print "$network \n";
     $tree->insert_network($network, $data);
 }
 
-# Write the database to disk.
-open my $fh, '>:raw', $filename;
-$tree->write_tree( $fh );
-close $fh;
+save();
+exit 1;
 
-say "$filename has now been created";
+### SUBROUTINES
+sub save {
+	open my $fh, '>:raw', $filename;
+	$tree->write_tree( $fh );
+	close $fh;
 
-sub asn {
-    my %format = (
-        autonomous_system_number => shift,
-        autonomous_system_organization => shift,
-    );
-    return \%format
+	say "$filename has now been created";
+	return;
 }
+
+__END__
+
+=pod
+
+=head1 create_asn
+
+Command sample - perl create_asn.pl --file asn.json
+
+=head1 SYNOPSIS
+
+perl create_asn.pl [options] [parameters] 
+ 
+Options:
+	--help            brief help message
+
+Parameters:
+	--file <path/to/asn.json>			Path to Custom asn data in JSON format.
+	--public <path/to/public.mmdb>		Path to MaxMind database.
+	--output <path/to/save.mmdb>		Output location to where custom mmdb is saved.
+
+=head1 DESCRIPTION
+
+Curates data from JSON datafile and optionally from a MaxMind Public GeoIP2-ASN Database Binary file to create a customized GeoIP
+mmdb binary file.
+
+=head2 OPTIONS
+
+=over 8
+
+=item B<--help|-h>
+
+Prints usage details
+
+=back
+
+=head2 PARAMETERS
+
+=over 8
+
+=item B<--file|-f>
+
+REQUIRED. Name of file to load custom asn data; Must be a valide json structure. Use jsonlint.com to confirm if necessary.
+
+=item B<--public|-p>
+
+OPTIONAL. Path to MaxMind GeoIP asn Database to incorporate into the custom database. If not provided, signifies that only data from
+<--file|-f> option will be considered
+
+=item B<--output|-o>
+
+OPTIONAL. Output location and file name where new custom mmdb will be saved. If none is provided DEFAULT is used. 
+DEFAULT => './GeoLite2-ASN-SAP.mmdb'
+
+=back
+
+=head1 DATASE FORMAT
+
+See C<< %types >> in source code for accepted JSON formating.
+
+=cut
