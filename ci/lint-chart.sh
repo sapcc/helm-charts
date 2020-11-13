327da65693500d89f57ee8fdc68deb92f2f0ee67
Arno Uhlig <arno.uhlig@sap.com>
2020-11-13 17:11:23 +0100

    update chart-test img

--- ci/lint-chart.sh
+++ ci/lint-chart.sh
@@ -7,7 +7,7 @@ if [ ! -f "$HELM_CHARTS/$1/Chart.yaml" ]; then
   exit 1
 fi
 
-docker run --rm -v $HELM_CHARTS:/charts -w /charts sapcc/chart-testing:v2.3.3-sapcc  ct lint \
+docker run --rm -v $HELM_CHARTS:/charts -w /charts sapcc/chart-testing:v3.0.0-rc.1-sapcc  ct lint \
   --chart-yaml-schema ci/chart_schema.yaml \
   --lint-conf ci/lintconf.yaml \
   --config ci/config.yaml --charts $1
