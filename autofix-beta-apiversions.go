b9e5d89d369f924ac4cdb7bf5126c99c0aa27440
Stefan Majewsky <stefan.majewsky@sap.com>
2020-11-13 16:56:59 +0100

    autofix-beta-apiversions.go: support multiple arguments

--- autofix-beta-apiversions.go
+++ autofix-beta-apiversions.go
@@ -37,40 +37,41 @@ var (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "usage: %s <yaml-template>\n", os.Args[0])
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: %s <yaml-template>...\n", os.Args[0])
 		os.Exit(1)
 	}
-	path := os.Args[1]
 
-	contents, err := ioutil.ReadFile(path)
-	must(err)
-	lines := strings.SplitAfter(string(contents), "\n")
+	for _, path := range os.Args[1:] {
+		contents, err := ioutil.ReadFile(path)
+		must(err)
+		lines := strings.SplitAfter(string(contents), "\n")
 
-	for idx, line := range lines {
-		apiVersionMatch := apiVersionRx.FindStringSubmatch(line)
-		if apiVersionMatch == nil {
-			continue
+		for idx, line := range lines {
+			apiVersionMatch := apiVersionRx.FindStringSubmatch(line)
+			if apiVersionMatch == nil {
+				continue
+			}
+			apiVersion := apiVersionMatch[1]
+			if !isDeprecatedAPIVersion[apiVersion] {
+				continue
+			}
+			kind := findNearbyKind(lines, idx)
+			if kind == "" {
+				fmt.Fprintf(os.Stderr, "%s:%d: cannot find Kind nearby\n", path, idx+1)
+				os.Exit(1)
+			}
+			newAPIVersion := correctAPIVersions[kind]
+			if newAPIVersion == "" {
+				fmt.Fprintf(os.Stderr, "%s:%d: do not know how to fix %s/%s\n", path, idx+1, apiVersion, kind)
+				os.Exit(1)
+			}
+			fmt.Printf("%s: autofixing %s/%s -> %s/%s\n", path, apiVersion, kind, newAPIVersion, kind)
+			lines[idx] = strings.Replace(line, apiVersion, newAPIVersion, 1)
 		}
-		apiVersion := apiVersionMatch[1]
-		if !isDeprecatedAPIVersion[apiVersion] {
-			continue
-		}
-		kind := findNearbyKind(lines, idx)
-		if kind == "" {
-			fmt.Fprintf(os.Stderr, "%s:%d: cannot find Kind nearby\n", path, idx+1)
-			os.Exit(1)
-		}
-		newAPIVersion := correctAPIVersions[kind]
-		if newAPIVersion == "" {
-			fmt.Fprintf(os.Stderr, "%s:%d: do not know how to fix %s/%s\n", path, idx+1, apiVersion, kind)
-			os.Exit(1)
-		}
-		fmt.Printf("%s: autofixing %s/%s -> %s/%s\n", path, apiVersion, kind, newAPIVersion, kind)
-		lines[idx] = strings.Replace(line, apiVersion, newAPIVersion, 1)
+
+		must(ioutil.WriteFile(path, []byte(strings.Join(lines, "")), 0666))
 	}
-
-	must(ioutil.WriteFile(path, []byte(strings.Join(lines, "")), 0666))
 }
 
 func must(err error) {
