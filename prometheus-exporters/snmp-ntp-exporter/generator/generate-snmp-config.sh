e13d8164e87bcc7debd640d59c091840c29384fc
Dimitris Tassopoulos <dimitrios.tassopoulos@sap.com>
2021-05-19 14:36:52 +0200

    Cleaned up not used routers in order not to duplicate pollling for devices that are already polled from the snmp_exporter

--- prometheus-exporters/snmp-ntp-exporter/generator/generate-snmp-config.sh
+++ prometheus-exporters/snmp-ntp-exporter/generator/generate-snmp-config.sh
@@ -8,10 +8,10 @@ fi
 
 if [ $1 = "all" ]; then
 
-   export modules=`ls  ../_snmp-exporter-*|awk -F- '{ print $3}'|awk -F. '{ print $1}'`
+   export modules=`ls  ../_snmp-ntp-exporter-*|awk -F- '{ print $3}'|awk -F. '{ print $1}'`
 
 else
-   export modules=`ls  ../_snmp-exporter-*|awk -F- '{ print $3}'|awk -F. '{ print $1}'`
+   export modules=`ls  ../_snmp-ntp-exporter-*|awk -F- '{ print $3}'|awk -F. '{ print $1}'`
    echo $modules |grep -qw $1
 
    if [ $? -eq 0 ] ; then
@@ -27,7 +27,7 @@ fi
 
 # cd
 #git clone https://github.com/sapcc/helm-charts.git
-# cd ./helm-charts/prometheus-exporters/snmp-exporter/generator/
+# cd ./helm-charts/prometheus-exporters/snmp-ntp-exporter/generator/
 
 
 mv /usr/share/snmp/mibs/CISCO-UNIFIED-COMPUTING-TC-MIB.mib /usr/share/snmp/ # This mib makes other generators fail...
@@ -51,40 +51,40 @@ for i in $modules;
             mv /usr/share/snmp/mibs/CISCO-UNIFIED-COMPUTING-TC-MIB.mib /usr/share/snmp/
         fi
 
-        mv -f ./snmp.yml ./_snmp-exporter-${i}.yaml.tmp
+        mv -f ./snmp.yml ./_snmp-ntp-exporter-${i}.yaml.tmp
         rm -d ./generator.yml
 
         if test -f "${i}-additional-oids.yaml"; then
-            awk -v f=$i '{ print; } /walk:/ { system ( "cat "f"-additional-oids.yaml" ) } \' _snmp-exporter-${i}.yaml.tmp  > ../_snmp-exporter-${i}.yaml
+            awk -v f=$i '{ print; } /walk:/ { system ( "cat "f"-additional-oids.yaml" ) } \' _snmp-ntp-exporter-${i}.yaml.tmp  > ../_snmp-ntp-exporter-${i}.yaml
         else
-            mv -f ./_snmp-exporter-${i}.yaml.tmp ../_snmp-exporter-${i}.yaml
+            mv -f ./_snmp-ntp-exporter-${i}.yaml.tmp ../_snmp-ntp-exporter-${i}.yaml
         fi
  
         if [[ "$i" =~ ^(f5mgmt|f5physical|f5customer)$ ]]; then
-            sed -i "s/- name: /- name: snmp_f5_/g" ../_snmp-exporter-${i}.yaml
+            sed -i "s/- name: /- name: snmp_f5_/g" ../_snmp-ntp-exporter-${i}.yaml
         else
-            sed -i "s/- name: /- name: snmp_${i}_/g" ../_snmp-exporter-${i}.yaml
+            sed -i "s/- name: /- name: snmp_${i}_/g" ../_snmp-ntp-exporter-${i}.yaml
         fi
  
         if test -f "${i}-additional-metrics.yaml"; then
-                cat ${i}-additional-metrics.yaml >> ../_snmp-exporter-${i}.yaml
-                rm -f ./_snmp-exporter-${i}.yaml.tmp
+                cat ${i}-additional-metrics.yaml >> ../_snmp-ntp-exporter-${i}.yaml
+                rm -f ./_snmp-ntp-exporter-${i}.yaml.tmp
         fi
 done
 
 
-if grep -q "arista:" ../_snmp-exporter-arista.yaml; then
-    sed -i '2d' ../_snmp-exporter-arista.yaml
+if grep -q "arista:" ../_snmp-ntp-exporter-arista.yaml; then
+    sed -i '2d' ../_snmp-ntp-exporter-arista.yaml
 fi
 
-if grep -q "n7k:" ../_snmp-exporter-n7k.yaml; then
-    sed -i '2d' ../_snmp-exporter-n7k.yaml
+if grep -q "n7k:" ../_snmp-ntp-exporter-n7k.yaml; then
+    sed -i '2d' ../_snmp-ntp-exporter-n7k.yaml
 fi
 
-if grep -q "n7k:" ../_snmp-exporter-n7kcontext.yaml; then
-    sed -i '2d' ../_snmp-exporter-n7kcontext.yaml
+if grep -q "n7k:" ../_snmp-ntp-exporter-n7kcontext.yaml; then
+    sed -i '2d' ../_snmp-ntp-exporter-n7kcontext.yaml
 fi
 
-if grep -q "asw:" ../_snmp-exporter-arista.yaml; then
-    sed -i '2d' ../_snmp-exporter-arista.yaml
+if grep -q "asw:" ../_snmp-ntp-exporter-arista.yaml; then
+    sed -i '2d' ../_snmp-ntp-exporter-arista.yaml
 fi
