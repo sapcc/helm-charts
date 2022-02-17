844ab904a5a41fff5b9711aaef4b0a83601c02a1
Jan Knipper <jan.knipper@sap.com>
2022-02-17 14:56:54 +0100

    Add concourse roles, ext lb, config

--- global/concourse-main/roles/set-team-roles.sh
+++ global/concourse-main/roles/set-team-roles.sh
@@ -3,7 +3,7 @@
 CONCOURSE_TARGET="${CONCOURSE_TARGET:-main}"
 FLY_BIN="${FLY_BIN:-fly-7.6.0}"
 
-for file in teams/*.yaml; do
+for file in $1/*.yaml; do
     TEAM=`echo ${file} | cut -d '/' -f2 | cut -d'.' -f1`
     echo "Setting roles for team ${TEAM} ..."
     ${FLY_BIN} set-team -t ${CONCOURSE_TARGET} -n ${TEAM} -c $file --non-interactive
