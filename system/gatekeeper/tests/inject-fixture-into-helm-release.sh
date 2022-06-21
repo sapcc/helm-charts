c0b4bac34bd967348dcaedbabe734c786b4cd1d8
Sandro Jäckel <sandro.jaeckel@sap.com>
2022-06-21 11:37:24 +0200

    system/gatekeeper: drop raw flag from yq
    yq-go does not understand this and defaults to raw

--- system/gatekeeper/tests/inject-fixture-into-helm-release.sh
+++ system/gatekeeper/tests/inject-fixture-into-helm-release.sh
@@ -10,12 +10,12 @@ fi
 fixture="$1"
 
 helm_release=almost-empty-chart/helm-release.yaml
-template="$(yq -r '.template' "$fixture")"
+template="$(yq '.template' "$fixture")"
 template_base64='| .chart.templates[].data = "'"$(echo "$template" | base64 -w0)"'" |  .manifest = "'"$template"'"'
 
 while read -r line; do
   # set both .chart.values and .config to emulate subcharts
   values+="| .chart.values.$line | .config.$line"
-done < <(yq -r '.values[] | to_entries[] | .key + " = \"" + .value + "\""' "$fixture")
+done < <(yq '.values[] | to_entries[] | .key + " = \"" + .value + "\""' "$fixture")
 
-yq -r '. | .data.release = "'"$(yq -r .data.release "$helm_release" | base64 -d | base64 -d | gunzip | jq ". ${values:-} $template_base64" | gzip | base64 -w0 | base64 -w0)"'"' "$helm_release"
+yq '. | .data.release = "'"$(yq .data.release "$helm_release" | base64 -d | base64 -d | gunzip | jq ". ${values:-} $template_base64" | gzip | base64 -w0 | base64 -w0)"'"' "$helm_release"
