69e4da4c22fa82cbe0a7606614cf7112c2e1b725
Birk Bohne <birk.bohne@sap.com>
2022-06-28 22:36:50 +0200

    init db and json logging added to entrypoint.sh - logging to stdout/stderr enforced

--- common/mariadb-galera/docker/bin/entrypoint.sh
+++ common/mariadb-galera/docker/bin/entrypoint.sh
@@ -1,7 +1,33 @@
 
 #!/usr/bin/env bash
-# This script prepares the startup and starts the startup script.
 BASE=/opt/${SOFTWARE_NAME}
 
-#exec /usr/bin/${SOFTWARE_NAME}
-exec /bin/sleep 3600
+declare -f initdb echoStdOut echoStdErr
+
+function echoStdOut {
+  printf "{\"@timestamp\":\"%s\",\"ecs.version\":\"1.6.0\",\"log.level\":\"info\",\"message\":\"%s\"}\n" "$(date +%Y.%m.%d-%H:%M:%S-%Z)" "$*" >>/dev/stdout
+}
+
+function echoStdErr {
+  printf "{\"@timestamp\":\"%s\",\"ecs.version\":\"1.6.0\",\"log.level\":\"error\",\"message\":\"%s\"}\n" "$(date +%Y.%m.%d-%H:%M:%S-%Z)" "$*" >>/dev/stderr
+}
+
+function initdb {
+  # check if the data folder already contains database structures
+  echoStdOut "Init databases if required"
+  if [ -d "${BASE}/data/mysql" ]; then
+		echoStdOut "Database structures already exist."
+    return
+  else
+    /usr/bin/mariadb-install-db --defaults-file=${BASE}/etc/my.cnf --basedir=/usr --skip-test-db
+    if [ $? -ne 0 ]; then
+      echoStdErr "Database initialization has been failed"
+      exit 1
+    fi
+  fi
+  echoStdOut "Init databases done"
+}
+
+initdb
+
+exec /usr/sbin/mariadbd --defaults-file=${BASE}/etc/my.cnf --basedir=/usr --skip-log-error
