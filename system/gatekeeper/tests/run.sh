3346d2eee00c6983cc6acb8d2f56ed3d9b9180d2
Stefan Majewsky <stefan.majewsky@sap.com>
2022-09-07 14:42:38 +0200

    gatekeeper: speed up `make check` significantly
    1. The rendering of Helm release fixtures moves into the Makefile, so
       fixtures will only get re-rendered when their inputs change.
    
    2. helm-manifest-parser and doop-image-checker are now run with the
       FAST_SHUTDOWN option, which shortens their shutdown grace period from
       10s to 100ms. We actually wait this out by sleeping for 500ms, so the
       final shutdown log messages do not appear after the next shell prompt
       anymore.

--- system/gatekeeper/tests/run.sh
+++ system/gatekeeper/tests/run.sh
@@ -5,12 +5,12 @@ cd "$(dirname "$0")"
 
 : "${HELM:=helm}"
 
-LOG_ALL_REQUESTS=1 helm-manifest-parser 127.0.0.1:8080 &
+FAST_SHUTDOWN=1 LOG_ALL_REQUESTS=1 helm-manifest-parser 127.0.0.1:8080 &
 pid_helm_manifest_parser=$!
-LOG_ALL_REQUESTS=1 doop-image-checker 127.0.0.1:8081 response-config.yaml &
+FAST_SHUTDOWN=1 LOG_ALL_REQUESTS=1 doop-image-checker 127.0.0.1:8081 response-config.yaml &
 pid_doop_image_checker=$!
 # shellcheck disable=SC2064
-trap "kill $pid_helm_manifest_parser $pid_doop_image_checker" INT TERM ERR EXIT
+trap "kill $pid_helm_manifest_parser $pid_doop_image_checker && sleep 0.5" INT TERM ERR EXIT
 
 (
   cd ..
@@ -23,10 +23,5 @@ trap "kill $pid_helm_manifest_parser $pid_doop_image_checker" INT TERM ERR EXIT
   [[ ! -d charts ]] && ${HELM} dep up >/dev/null
   ${HELM} template gatekeeper-config . --values ci/test-values.yaml --output-dir ../gatekeeper/tests/rendered-chart >/dev/null
 )
-
-for file in fixtures/*/*.in.yaml; do
-  out_file="$(dirname "$file")/$(basename "$file" '.in.yaml').out.yaml"
-  ./inject-fixture-into-helm-release.sh "$file" >"$out_file"
-done
 
 gator verify -v .
