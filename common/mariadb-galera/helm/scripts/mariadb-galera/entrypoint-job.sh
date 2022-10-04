4190eb41b3a588343b0396511383d7768bcf57a2
Birk Bohne <birk.bohne@sap.com>
2022-10-04 14:57:29 +0200

    MariaDB user management reused for ProxySQL - to keep the users in both components in sync - chart version bumped

--- common/mariadb-galera/helm/scripts/mariadb-galera/entrypoint-job.sh
+++ common/mariadb-galera/helm/scripts/mariadb-galera/entrypoint-job.sh
@@ -28,6 +28,8 @@ setupuser "${MARIADB_MONITORING_USER}" "${MARIADB_MONITORING_PASSWORD}" 'mysql_e
 
 {{- /* Load additional configuration files for MariaDB to be processed by the job container */}}
 {{- $mariadbconfigs := $.Files.Get "config/mariadb-galera/values.yaml" | fromYaml }}
+{{- $usernameEnvVarFound := false }}
+{{- $passwordEnvVarFound := false }}
 {{- range $mariadbconfigKey, $mariadbconfigValue := required "A valid 'configs.' structure is required from config/mariadb-galera/values.yaml" $mariadbconfigs.configs }}
   {{- if $mariadbconfigValue.enabled}}
     {{- $configfile := $.Files.Get (printf "config/mariadb-galera/%s/%s" $mariadbconfigValue.type $mariadbconfigValue.file) | fromYaml }}
@@ -41,13 +43,27 @@ setuprole {{ $mariadbconfigValue.name | quote }} {{ $configfile.privileges | joi
       {{- end }}
     {{- else if eq $mariadbconfigValue.type "user" }}
       {{- range $hostnameKey, $hostnameValue := required (printf "A valid 'hostnames.' structure is required in config/mariadb-galera/%s/%s" $mariadbconfigValue.type $mariadbconfigValue.file) $configfile.hostnames }}
+        {{- $usernameEnvVarFound = false }}
+        {{- $passwordEnvVarFound = false }}
         {{- range $envKey, $envValue := $.Values.env }}
-          {{- if and (eq $envKey ($configfile.password | trimAll "${}")) ($envValue) }}
-setupuser {{ $configfile.username | quote }} {{ $configfile.password | quote }} {{ $configfile.role | quote }} {{ $configfile.maxconnections | quote }} {{ $hostnameValue | quote }}
-          {{- else }}
-            {{- /* fail (printf "%s environment variable not defined, but required for config/mariadb-galera/%s/%s" ($configfile.password | trimAll "${}" | quote) $mariadbconfigValue.type $mariadbconfigValue.file) */}}
+          {{- if (has "job" $envValue.containerType) }}
+            {{- if and (eq $envKey ($configfile.username | trimAll "${}")) ($envValue) }}
+              {{- $usernameEnvVarFound = true }}
+            {{- end }}
+            {{- if and (eq $envKey ($configfile.password | trimAll "${}")) ($envValue) }}
+              {{- $passwordEnvVarFound = true }}
+            {{- end }}
           {{- end }}
         {{- end }}
+        {{- if and $usernameEnvVarFound $passwordEnvVarFound }}
+setupuser {{ $configfile.username | quote }} {{ $configfile.password | quote }} {{ $configfile.role | quote }} {{ $configfile.maxconnections | quote }} {{ $hostnameValue | quote }}
+        {{- end }}
+      {{- end }}
+      {{- if (not $usernameEnvVarFound) }}
+        {{- fail (printf "%s environment variable not defined, but required for config/mariadb-galera/%s/%s" ($configfile.username | trimAll "${}" | quote) $mariadbconfigValue.type $mariadbconfigValue.file) }}
+      {{- end }}
+      {{- if (not $passwordEnvVarFound) }}
+        {{- fail (printf "%s environment variable not defined, but required for config/mariadb-galera/%s/%s" ($configfile.password | trimAll "${}" | quote) $mariadbconfigValue.type $mariadbconfigValue.file) }}
       {{- end }}
     {{- end }}
   {{- end }}
