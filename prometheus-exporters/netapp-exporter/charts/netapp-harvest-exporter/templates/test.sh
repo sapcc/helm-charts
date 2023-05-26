ea014a67ad4fd0d75a5d9060e423e1ea12697d94
Maurice Escher <maurice.escher@sap.com>
2023-05-26 11:13:51 +0200

    [netapp-exporter] remove test script
    came with 9254ec4707d5c2f74573a696bb8ee587058f7e90

--- prometheus-exporters/netapp-exporter/charts/netapp-harvest-exporter/templates/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-#
-# fetch all prommetheus metrics with name netapp_poller_status using curl
-metrics=curl -s -H "Accept: application/json" -H "Content-Type: application/json" http://localhost:9090/api/v1/query?query=netapp_poller_status | jq -r '.data.result[] | .metric.node + " " + .metric.cluster + " " + .metric.poller + " " + .value[1]' | sort -k 2,2 -k 1,1
