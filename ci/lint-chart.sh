e0293a9f476f8bb81536033e530748e8902e18ff
Birk Bohne <birk.bohne@sap.com>
2023-06-22 12:35:04 +0200

    chart-testing bumped to 3.8.0 - to fix linting problems - to use recent versions of the related tools

--- ci/lint-chart.sh
+++ ci/lint-chart.sh
@@ -9,5 +9,5 @@ fi
 
 docker run \
   --rm --network host --env HOME=/tmp --user $(id -u):$(id -g) -v $HELM_CHARTS:/charts -w /charts \
-  sapcc/chart-testing:v3.0.0-rc.1-sapcc ct lint \
+  sapcc/chart-testing:v3.8.0-sapcc ct lint \
   --chart-yaml-schema ci/chart_schema.yaml --lint-conf ci/lintconf.yaml --config ci/config.yaml --charts $1
