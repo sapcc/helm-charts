08b58cfac27d13d8e2b8a4e477b92e12f4c35fb7
Olaf Heydorn <olaf.heydorn@sap.com>
2023-11-14 09:23:07 +0100

    [snmp-exporter] adding f5archer to generator script

--- prometheus-exporters/snmp-exporter/generator/generate-snmp-config.sh
+++ prometheus-exporters/snmp-exporter/generator/generate-snmp-config.sh
@@ -63,7 +63,7 @@ for i in $modules;
             mv -f ./_snmp-exporter-${i}.yaml.tmp ../_snmp-exporter-${i}.yaml
         fi
  
-        if [[ "$i" =~ ^(f5mgmt|f5physical|f5customer|f5gtm)$ ]]; then
+        if [[ "$i" =~ ^(f5mgmt|f5physical|f5customer|f5gtm|f5archer)$ ]]; then
             sed -i "s/- name: /- name: snmp_f5_/g" ../_snmp-exporter-${i}.yaml
         else
             sed -i "s/- name: /- name: snmp_${i}_/g" ../_snmp-exporter-${i}.yaml
