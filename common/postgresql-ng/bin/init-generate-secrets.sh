41ab391ba6a818c0e65c7b5ada5f6912828cad5f
Sandro Jäckel <sandro.jaeckel@sap.com>
2023-12-11 15:30:59 +0100

    postgres-ng: don't create password with trailing new line, really delete secrets

--- common/postgresql-ng/bin/init-generate-secrets.sh
+++ common/postgresql-ng/bin/init-generate-secrets.sh
@@ -15,18 +15,20 @@ for USER in ${USERS:-}; do
   fi
 
   # create new secret with randomly generated password
-  echo "
+  # NOTE: make sure that the generated password contains no newline
+  echo -n "
     apiVersion: v1
     kind: Secret
     metadata:
       name: $SECRET
       ownerReferences:
         - apiVersion: v1
+          blockOwnerDeletion: true
           kind: Deployment
           name: $RELEASE-postgresql
           uid: $(kubectl get deployment "$RELEASE-postgresql" -o jsonpath='{.metadata.uid}')
     data:
-      postgres-password: $(head -c 30 </dev/urandom | base64 | base64)
+      postgres-password: $(head -c 30 </dev/urandom | base64 -w0 | base64 -w0)
   " > secret.yaml
   kubectl create -f secret.yaml
 done
