29e10c305a6cf09d03168f0b04be14fdf7257ffe
Jan Knipper <jan.knipper@sap.com>
2024-01-30 15:42:59 +0100

    Raise identity cert lifetime

--- system/servicemesh/images.sh
+++ system/servicemesh/images.sh
@@ -1,6 +1,7 @@
 LINKERD_VERSION=stable-2.14.9
 LINKERD_PROXY_INIT_VERSION=v2.2.3
 LINKERD_EXTENSION_INIT_VERSION=v0.1.0
+PROMETHEUS_VERSION=v2.48.0
 
 docker build --network=host -t keppel.eu-de-1.cloud.sap/ccloud/linkerd/proxy:$LINKERD_VERSION --build-arg IMAGE="cr.l5d.io/linkerd/proxy:$LINKERD_VERSION" - < Dockerfile
 docker push keppel.eu-de-1.cloud.sap/ccloud/linkerd/proxy:$LINKERD_VERSION
@@ -28,3 +29,5 @@ docker push keppel.eu-de-1.cloud.sap/ccloud/linkerd/web:$LINKERD_VERSION
 
 docker build --network=host -t keppel.eu-de-1.cloud.sap/ccloud/linkerd/extension-init:$LINKERD_EXTENSION_INIT_VERSION --build-arg IMAGE="cr.l5d.io/linkerd/extension-init:$LINKERD_EXTENSION_INIT_VERSION" - < Dockerfile
 docker push keppel.eu-de-1.cloud.sap/ccloud/linkerd/extension-init:$LINKERD_EXTENSION_INIT_VERSION
+
+docker pull keppel.eu-de-1.cloud.sap/ccloud-dockerhub-mirror/prom/prometheus:$PROMETHEUS_VERSION
