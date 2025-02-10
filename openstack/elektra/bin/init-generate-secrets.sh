2b66a0c16ab0e00b00d574b9ce6dd370d291cc24
Arturo Reuschenbach Puncernau <reuschenbach@gmail.com>
2025-02-10 14:15:45 +0100

    feat(elektra): creates a random rails token if non existing

---
+++ openstack/elektra/bin/init-generate-secrets.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env ash
+# shellcheck shell=ash
+# shellcheck disable=SC3010
+
+# This is the entrypoint script for the "generate-secrets" init container of the elektra pod.
+
+set -eou pipefail
+[[ ${DEBUG:-} != false ]] && set -x
+
+OLD_SECRET="$RELEASE"
+SECRET="$RELEASE-rails-token"
+
+# if we already have a secret, we can stop here
+if [[ "$(kubectl get secrets "$SECRET" --ignore-not-found)" != "" ]]; then
+  continue
+fi
+
+if kubectl get secret "$OLD_SECRET" >/dev/null 2>&1; then
+  EXISTING_TOKEN=$(kubectl get secret "$OLD_SECRET" -o jsonpath="{.data.$KEY}" 2>/dev/null || echo "")
+  if [[ -n "$EXISTING_TOKEN" ]]; then
+    # Decode the existing token (Kubernetes stores secrets in base64)
+    SECRET_KEY_BASE=$(echo "$EXISTING_TOKEN" | base64 -d)
+  else
+    SECRET_KEY_BASE=$(head -c 64 /dev/urandom | base64 | tr -d '\n' | cut -c1-64)
+  fi
+fi
+
+# create new secret with randomly generated token
+echo -n "
+  apiVersion: v1
+  kind: Secret
+  metadata:
+    name: $SECRET
+    ownerReferences:
+      - apiVersion: apps/v1
+        blockOwnerDeletion: true
+        kind: Deployment
+        name: $DEPLOYMENT_NAME
+        uid: $(kubectl get deployment "$DEPLOYMENT_NAME" -o jsonpath='{.metadata.uid}')
+  data:
+    token: $(SECRET_KEY_BASE)
+" > secret.yaml
+  kubectl create -f secret.yaml
+
