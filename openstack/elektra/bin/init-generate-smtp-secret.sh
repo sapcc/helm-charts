4d76aae7e7d79ef6aeb8aed5c76e15325b656a27
Arturo Reuschenbach Puncernau <reuschenbach@gmail.com>
2025-02-24 17:12:17 +0100

    create credentials

--- openstack/elektra/bin/init-generate-smtp-secret.sh
+++ openstack/elektra/bin/init-generate-smtp-secret.sh
@@ -6,41 +6,38 @@
 set -eou pipefail
 [[ ${DEBUG:-false} != false ]] && set -x
 
+export OS_AUTH_TYPE=v3applicationcredential
 SECRET="elektra-smtp"
 
-# If we already have a secret, we can stop here
+# If we already have a secret, we can extract the ec2 access key
 if [[ -n "$(kubectl-v1.32.1 get secrets "$SECRET" --ignore-not-found)" ]]; then
-  exit 0
+  EC2_ACCESS_KEY=$(kubectl-v1.32.1 get secret "$SECRET" -o jsonpath='{.data.ec2_access_key}' | base64 --decode)
+  # Remove secret and ec2 credentials
+  kubectl-v1.32.1 delete secret "$SECRET"
+  openstack ec2 credentials delete --user dashboard --user-domain default $EC2_ACCESS_KEY
 fi
-
-# Step 0: Set env OS_AUTH_TYPE
-export OS_AUTH_TYPE=v3applicationcredential
 
 # Step 1: List ec2 credentials
 echo "Listing ec2 credentials..."
 openstack ec2 credentials list --user dashboard --user-domain default
 echo "Listing ec2 credentials done."
 
-# Step 2: Delete all ec2 credentials
-
 # Step 3: Create ec2 credentials
-# echo "Creating EC2 credentials..."
-# EC2_CREDS=$(openstack ec2 credentials create --user dashboard --user-domain default --project master --project-domain ccadmin -f json)
-# ACCESS_KEY=$(echo "$EC2_CREDS" | jq -r '.access')
-# SECRET_KEY=$(echo "$EC2_CREDS" | jq -r '.secret')
-# echo "EC2 credentials created."
+echo "Creating EC2 credentials..."
+EC2_CREDS=$(openstack ec2 credentials create --user dashboard --user-domain default --project master --project-domain ccadmin -f json)
+ACCESS_KEY=$(echo "$EC2_CREDS" | jq -r '.access')
+SECRET_KEY=$(echo "$EC2_CREDS" | jq -r '.secret')
+echo "EC2 credentials created."
 
 # Step 4: Authenticate and get OpenStack token
 echo "Generating SMTP credentials..."
-# SMTP_OUTPUT=$(cronuscli smtp credentials --ec2-access "$ACCESS_KEY" --ec2-secret "$SECRET_KEY")
-SMTP_OUTPUT=$(cronuscli smtp credentials --ec2-access 123 --ec2-secret 123 --base64)
+SMTP_OUTPUT=$(cronuscli smtp credentials --ec2-access "$ACCESS_KEY" --ec2-secret "$SECRET_KEY")
 USERNAME=$(echo "$SMTP_OUTPUT" | grep -oP 'Username:\s+\K.*')
 PASSWORD=$(echo "$SMTP_OUTPUT" | grep -oP 'Password:\s+\K.*')
 echo "SMTP credentials obtained"
 
-# export OS_TOKEN=$(openstack token issue -f value -c id)
-
-# # Step 5: create new secret with randomly generated token
+# Step 5: create new secret with randomly generated token
+# ec2_access_key: $ACCESS_KEY
 echo -n "
   apiVersion: v1
   kind: Secret
@@ -53,6 +50,7 @@ echo -n "
         name: elektra
         uid: $(kubectl-v1.32.1 get deployment elektra -o jsonpath='{.metadata.uid}')
   data:
+    ec2_access_key: $ACCESS_KEY
     username: $USERNAME
     password: $PASSWORD
 " > secret.yaml
