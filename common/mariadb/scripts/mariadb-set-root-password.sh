403ad6134fb72f5ab911c8e6c66dcd1def6e6161
Vlad Gusev <vlad.esten@gmail.com>
2025-02-27 17:25:45 +0200

    [mariadb] Use pod-local unix_socket auth for root@localhost

--- common/mariadb/scripts/mariadb-set-root-password.sh
+++ common/mariadb/scripts/mariadb-set-root-password.sh
@@ -16,7 +16,7 @@ MYSQL_PASSWORD="${mysql_pass:-$MYSQL_ROOT_PASSWORD}"
 
 cat > /var/lib/initdb/init.sql <<-EOSQL || true
     SET SESSION sql_log_bin=0;
-    ALTER USER 'root'@'localhost' IDENTIFIED BY '$(escape_special "${MYSQL_PASSWORD}")';
+    ALTER USER 'root'@'localhost' IDENTIFIED VIA unix_socket;
     ALTER USER 'root'@'%' IDENTIFIED BY '$(escape_special "${MYSQL_PASSWORD}")';
     FLUSH PRIVILEGES;
 EOSQL
