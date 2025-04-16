163b1f47b634a34223ba58a9f9041046a55e4a59
Sandro Jäckel <sandro.jaeckel@sap.com>
2025-04-16 17:59:11 +0200

    postgresql-ng: simplify process_sql()

--- common/postgresql-ng/docker/entrypoint.sh
+++ common/postgresql-ng/docker/entrypoint.sh
@@ -149,7 +149,7 @@ for data in $(find /var/lib/postgresql/ -mindepth 1 -maxdepth 1 -type d -not -na
 
     # start postgres and load current hba conf
     start_postgres
-    PGDATABASE='' process_sql --dbname postgres -c "SELECT pg_reload_conf()"
+    PGDATABASE='postgres' process_sql -c "SELECT pg_reload_conf()"
 
     # we need to retry loop here a bit because the k8s service, through which pgbackup must go, is probably not yet up
     for i in {1..60}; do
@@ -194,7 +194,7 @@ echo -e "host  all  all  all  $PGAUTHMETHOD\n" >>"$PGDATA/pg_hba.conf"
 # update postgres.conf
 cp /etc/postgresql/postgresql.conf "$PGDATA/postgresql.conf"
 start_postgres
-PGDATABASE='' process_sql --dbname postgres -c "SELECT pg_reload_conf()"
+PGDATABASE='postgres' process_sql -c "SELECT pg_reload_conf()"
 
 # there might be some extensions which we need to enable
 if [[ -f /var/lib/postgresql/update_extensions.sql ]]; then
@@ -210,7 +210,7 @@ fi
 # if a new db was initted, create the database inside of it and run init scripts
 if [[ $created_db == true ]]; then
   # shellcheck disable=SC2097,SC2098 # false positive
-  PGDATABASE='' process_sql --dbname postgres --set db="$PGDATABASE" <<-'EOSQL'
+  PGDATABASE='postgres' process_sql --set db="$PGDATABASE" <<-'EOSQL'
     CREATE DATABASE :"db";
   EOSQL
 
@@ -224,7 +224,7 @@ fi
 # ensure that the configured password matches the password in the database
 # this was required when upgrading the password hashing algorithm from md5 to scram-sha-256 which was the case when eg. updating from 9.5 to 15
 # this also allows password rotations with restarts
-PGDATABASE='' process_sql --dbname postgres --set user="$PGUSER" --set password_encryption="$postgres_auth_method" --set password="$PGPASSWORD" <<-'EOSQL'
+PGDATABASE='postgres' process_sql --set user="$PGUSER" --set password_encryption="$postgres_auth_method" --set password="$PGPASSWORD" <<-'EOSQL'
   SET password_encryption = :'password_encryption';
   ALTER USER :user WITH PASSWORD :'password';
 EOSQL
