4f32d4d8b81019880cc9a3121b252710ae060003
Vladislav Gusev <vlad.esten@gmail.com>
2026-03-02 17:27:23 +0200

    [mariadb] add .Values.client_tls_option
    Add .Values.client_tls_option for cronjobs
    and rename-check-constraints job
    Defaults to "--skip-ssl-verify-server-cert"

--- common/mariadb/scripts/mariadb-cronjob-maintenance.sh
+++ common/mariadb/scripts/mariadb-cronjob-maintenance.sh
@@ -6,10 +6,10 @@ set -o pipefail
 source /usr/bin/common-functions.sh
 
 declare mysqlOpts
-mysqlOpts="--protocol=tcp --host={{ include "fullName" . }} --user=root --password=${MYSQL_ROOT_PASSWORD} --database=mysql --wait --connect-timeout=5 --reconnect --batch"
+mysqlOpts="${MARIADB_TLS_OPTION} --protocol=tcp --host={{ include "fullName" . }} --user=root --password=${MYSQL_ROOT_PASSWORD} --database=mysql --wait --connect-timeout=5 --reconnect --batch"
 
 function listTables {
-  mysql ${mysqlOpts} --skip-column-names --execute="SELECT CONCAT('\`',table_schema,'\`.\`',table_name,'\`') FROM information_schema.tables WHERE table_schema NOT IN ('information_schema','performance_schema','mysql','sys','innodb') AND engine IS NOT NULL;"
+  mariadb ${mysqlOpts} --skip-column-names --execute="SELECT CONCAT('\`',table_schema,'\`.\`',table_name,'\`') FROM information_schema.tables WHERE table_schema NOT IN ('information_schema','performance_schema','mysql','sys','innodb') AND engine IS NOT NULL;"
   if [ "$?" -ne 0 ]; then
     logerror "${FUNCNAME[0]}" "table query failed"
     exit 1
@@ -25,7 +25,7 @@ function analyzeTable {
     exit 1
   else
     loginfo "${FUNCNAME[0]}" "analyze table ${tblName}"
-    MYSQL_RESPONSE=$(mysql ${mysqlOpts} --execute="ANALYZE TABLE ${tblName} PERSISTENT FOR ALL;")
+    MYSQL_RESPONSE=$(mariadb ${mysqlOpts} --execute="ANALYZE TABLE ${tblName} PERSISTENT FOR ALL;")
     MYSQL_STATUS=$?
     MYSQL_QUERY_STATUS=$(echo "${MYSQL_RESPONSE}" | grep "Error" | wc -l)
     {{- if $.Values.job.maintenance.function.analyzeTable.verbose }}
