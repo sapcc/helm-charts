8b7ba330147cfbd16a3c7f50aea6113470e51736
Stefan Majewsky <stefan.majewsky@sap.com>
2026-03-03 11:04:24 +0100

    keppel: add CronJob to generate local issuer keys
    The ultimate goal here is to remove the current manual process of
    generating/rotating those issuer keys. For now, this only creates the
    keys, without actually using them. Once this has been deployed
    everywhere and run at least once, I will send a followup PR to cut over
    to the new keys.

---
+++ openstack/keppel/files/write-issuer-key.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env ash
+# shellcheck shell=ash
+# shellcheck disable=SC3010
+
+# This is the entrypoint script for the "write-secret" container of the cronjob "keppel-generate-issuer-key".
+# The previous contents of the secret (if any) are given in `/keys`.
+# The newly generated issuer key is in `/work/current-key.pem`.
+
+set -eou pipefail
+[[ ${DEBUG:-} != false ]] && set -x
+
+if [[ -f /keys/current-key.pem ]]; then
+  cp /keys/current-key.pem /work/previous-key.pem
+else
+  # On first run, the secret does not exist yet, so we do not have a previous key.
+  # Putting the same key twice is a safe fallback that allows keeping the same secret structure in this case.
+  cp /work/current-key.pem /work/previous-key.pem
+fi
+
+echo -n "
+  apiVersion: v1
+  kind: Secret
+  metadata:
+    name: $SECRET_NAME
+    ownerReferences:
+      - apiVersion: batch/v1
+        blockOwnerDeletion: true
+        kind: CronJob
+        name: $CRONJOB_NAME
+        uid: $(kubectl get cronjob "$CRONJOB_NAME" -o jsonpath='{.metadata.uid}')
+  data:
+    current-key.pem:  $(cat /work/current-key.pem  | base64 -w0)
+    previous-key.pem: $(cat /work/previous-key.pem | base64 -w0)
+" > /work/secret.yaml
+
+kubectl create -f /work/secret.yaml
